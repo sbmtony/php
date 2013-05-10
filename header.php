2cf1982fed9917b60a4cb49824f6e071ed59d87c
sbmtony <tony@sbmcrusher.net>
2013-05-10 19:12:41 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo isset ($_GET["method"])?($_GET["method"]):'上传，修改，编辑'; ?> -- PHP 学习</title>
+</head>
+
+<body>
+
+<hr />
